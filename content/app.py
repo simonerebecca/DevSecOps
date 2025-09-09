c75fdc9d8bfbc46323ed900665b6d92c2a212388
simoneverhagen <simoneverhagen@live.nl>
2025-09-09 16:00:34 +0200

    fix: Add test header to main page for visual emphasis

--- content/app.py
+++ content/app.py
@@ -33,7 +33,8 @@ def check_authentication():
 @app.route("/")
 def index():
     quotes = db.execute("select id, text, attribution from quotes order by id").fetchall()
-    return templates.main_page(quotes, request.user_id, request.args.get('error'))
+    html = "<h1 style='color:#ff69b4; text-align:center; font-size:64px'>TESTTEST!!!</h1>"
+    return html + templates.main_page(quotes, request.user_id, request.args.get('error'))
 
 
 # The quote comments page
